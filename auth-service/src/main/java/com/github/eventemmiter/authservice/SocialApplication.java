acf94daa8883d83b7e8f56fe440dc43392ece7b6
solairerove <hax2033@gmail.com>
2018-05-21 12:03:13 +0300

    feature: update relative path for webjars

--- auth-service/src/main/java/com/github/eventemmiter/authservice/SocialApplication.java
+++ auth-service/src/main/java/com/github/eventemmiter/authservice/SocialApplication.java
@@ -3,12 +3,35 @@ package com.github.eventemmiter.authservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @SpringBootApplication
 @EnableOAuth2Sso
-public class SocialApplication {
+@RestController
+public class SocialApplication extends WebSecurityConfigurerAdapter {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SocialApplication.class, args);
-	}
+    @RequestMapping("/user")
+    public Principal user(final Principal principal) {
+        return principal;
+    }
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/**").authorizeRequests()
+                .antMatchers("/", "/login**", "/webjars/**", "/error**").permitAll()
+                .anyRequest().authenticated()
+                .and().logout().logoutSuccessUrl("/").permitAll()
+                .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialApplication.class, args);
+    }
 }
