6db20947d5be502492ed0a3dc3526756497f7659
solairerove <hax2033@gmail.com>
2018-05-21 11:49:13 +0300

    feature: facebook oauth simple social login

--- auth-service/src/main/java/com/github/eventemmiter/authservice/SocialApplication.java
+++ auth-service/src/main/java/com/github/eventemmiter/authservice/SocialApplication.java
@@ -2,11 +2,13 @@ package com.github.eventemmiter.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
-public class AuthServiceApplication {
+@EnableOAuth2Sso
+public class SocialApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AuthServiceApplication.class, args);
+		SpringApplication.run(SocialApplication.class, args);
 	}
 }
